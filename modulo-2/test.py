5a56e0582a99edbb8a3f46cca0d7eb8a0493c80b
alejandro.zarate1 <alejandro.zarate@jalisco.gob.mx>
2023-09-20 14:22:43 -0600

    Script para probar detecciones de senias

--- modulo-2/test.py
+++ modulo-2/test.py
@@ -1,3 +1,22 @@
-import torch
+from ultralytics import YOLO
+import cv2
 
-print(torch.cuda.is_available())
+model = YOLO('./best.pt')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    predict = model.predict(frame, imgsz=640, conf=0.85)
+
+    result = predict[0].plot()
+
+    cv2.imshow("Conduccion", frame)
+
+    if cv2.waitKey == 27:
+        break
+
+cap.release()
+cap.destoryAllWindows()
+
